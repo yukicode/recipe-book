339e0db804a7713b212a1ec257c64b8e0a2084ca
yukicode <yolandaapply@gmail.com>
2017-01-21 21:16:50 -0800

    style shopping list

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -6,26 +6,28 @@ export class ShoppingListService {
   private _ingredients: Ingredient[] = [];
   private _names: string[] = [];
   private _index: number[] = [];
+  private _ingredientString: string[] = [];
 
   getList(): Ingredient[] {
     return this._ingredients;
   }
 
   getListString(): string[] {
-    var j=0;
-    var result = [];
-    for(var i=0; i<this._ingredients.length; i++){
-      if(i===this._index[j]){
-        result.push(this._names[j++]);
-      }
-      result.push("✤    " + this._ingredients[i].name + ", " + this._ingredients[i].amount);
-    }
-    return result;
+    return this._ingredientString;
   }
 
   addToList(items: Ingredient[], recipeName: string): void {
     this._index.push(this._ingredients.length);
     this._names.push(recipeName);
     Array.prototype.push.apply(this._ingredients, items);
+
+    this._ingredientString.push(recipeName);
+    for(var i=0; i<items.length; i++){
+      this._ingredientString.push("✤    " + this._ingredients[i].name + ", " + this._ingredients[i].amount);
+    }
+  }
+
+  deleteIngredient(i: number): void{
+    this._ingredientString.splice(i, 1);
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Ingredient} from '../ingredient';
-import {ShoppingListService} from '../services/shopping-list.service';
+import { Ingredient } from '../ingredient';
+import { ShoppingListService } from '../services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,11 +8,48 @@ import {ShoppingListService} from '../services/shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  items: Ingredient[];
-  constructor(private shoppingListService : ShoppingListService) { }
+  items: string[];
+  isEditing: boolean[] = [];
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
-    this.items = this.shoppingListService.getList();
+    this.items = this.shoppingListService.getListString();
+    for (var i = 0; i < this.items.length; i++) {
+      this.isEditing[i] = false;
+    }
+    //Fot testing
+    if (!this.items || this.items.length === 0) {
+      this.items.push("Ingredient 1, amount");
+      this.items.push("Ingredient 2, amount");
+      this.items.push("Ingredient 3, amount");
+      this.items.push("Ingredient 4, amount");
+      this.isEditing.push(false);
+      this.isEditing.push(false);
+      this.isEditing.push(false);
+      this.isEditing.push(false);
+    }
+  }
+
+  delete(i: number): void {
+    this.shoppingListService.deleteIngredient(i);
+    this.isEditing.splice(i, 1);
+  }
+
+  edit(i: number): void {
+    if (!this.isEditing[i]) {
+      this.isEditing[i] = true;
+    }
+  }
+
+  save(i: number): void {
+    this.isEditing[i] = false;
+    return;
+  }
+
+  getName(i:string): string {
+    console.log("called");
+    return "called";
   }
 
 }
